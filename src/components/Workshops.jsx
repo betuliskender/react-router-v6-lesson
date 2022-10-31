a32560db30093b5ca782683b05ab01edba905a5e
Betül Iskender <betul.iskender@outlook.dk>
2022-10-31 15:42:27 +0100

    lesson 1 done

--- src/components/Workshops.jsx
+++ src/components/Workshops.jsx
@@ -1,7 +1,17 @@
+import {getWorkshops} from "../api.js"
+
 export default function Workshops() {
+
+  const workshops = getWorkshops()
   return (
       <div className="container">
           <h1>Workshop Categories</h1>
+          {workshops.map(workshop =>
+              <ul key={workshop.id}>
+                <li>{workshop.name}</li>
+              </ul>
+            )}
+
       </div>
   );
 }
